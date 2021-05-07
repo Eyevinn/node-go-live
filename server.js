578d898aa8ac4c426fb430f1ba8ab8060c3ea51d
birme <jonas.birme@eyevinn.se>
2021-05-07 13:37:30 +0200

    wip: ability to override role_arn in reference implementation

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ const { GoLiveApiServer } = require("./index.js");
 const server = new GoLiveApiServer({
   aws_access_key_id: process.env.AWS_ACCESS_KEY_ID,
   aws_secret_access_key: process.env.AWS_SECRET_ACCESS_KEY,
-  role_arn: "arn:aws:iam::590877988961:role/AllowMediaLiveAccessRole"
+  role_arn: process.env.AWS_ROLE_ARN || "arn:aws:iam::590877988961:role/AllowMediaLiveAccessRole"
 });
 
 try {
